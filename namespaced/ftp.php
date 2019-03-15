dcee933a1919c1957093d05995eff170f11f0c67
reyazilim <mehmet@reyazilim.com>
2019-03-15 22:58:03 +0300

    no message

--- namespaced/ftp.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Detection;
-
-require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'ftp.php';
-
-class Ftp extends \ftp {}
-
-
