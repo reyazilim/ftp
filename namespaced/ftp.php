2ea3c97762da33f80d1510d6ed8933c7413a5c50
reyazilim <mehmet@reyazilim.com>
2019-03-15 22:36:08 +0300

    main

---
+++ namespaced/ftp.php
@@ -0,0 +1,7 @@
+<?php
+namespace Detection;
+
+require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'ftp.php';
+
+class Ftp extends \ftp {}
+
